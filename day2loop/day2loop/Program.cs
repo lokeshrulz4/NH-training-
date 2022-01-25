1975df4c9eda8e5dea14274484e41ce7379d3fc1
lokeshrulz4 <lokeshrulz4@gmail.com>
2022-01-25 17:43:09 +0530

    date  of submission

---
+++ day2loop/day2loop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day2loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+        for(int i=1;i<6;i++)
+            {
+            Console.WriteLine("hello world{i}");
+            }
+        }
+    }
+}
+
---
+++ forloop1/forloop1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forloop
+{
+    internal class Program
+        static void Main(string[] args)
+    {
+        for (int i = 1; i < 6; i++)
+        {
+            Console.WriteLine("hello world{i}");
+        }
+        }
+    }
+}
