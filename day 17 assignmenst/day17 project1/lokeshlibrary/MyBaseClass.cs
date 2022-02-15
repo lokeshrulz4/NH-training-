8292fc41df24fbb67ba42451743bedbba8cad288
lokeshrulz4 <lokeshrulz4@gmail.com>
2022-02-16 00:14:57 +0530

    day17 assignments

---
+++ day 17 assignmenst/day17 project1/lokeshlibrary/MyBaseClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lokeshlibrary
+{
+    public class MyBaseClass
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        protected internal int e;
+
+
+
+        public void MyBaseClassMethod()
+        {
+            a = 10;
+            b = 20;
+            c = 20;
+            d = 40;
+            e = 60;
+        }
+    }
+
+
+    public class MyDerivedClass : MyBaseClass
+    {
+        public void MyDerivedClassMethod()
+        {
+            int a = 10;
+           // b = 20;
+            c = 20;
+            d = 40;
+            e = 60;
+        }
+    }
+
+
+    public class MyOtherClass
+    {
+        public void MyOtherClassMethod()
+        {
+            MyBaseClass mb = new MyBaseClass();
+            mb.a = 10;
+            //mb.b = 20;
+            //mb.c = 20;
+            mb.d = 40;
+            mb.e = 60;
+        }
+        
+    }
+}
+            
+
+
+
+    
---
+++ day 17 assignmenst/day17 project1/publiclibrary/MYDerivedClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lokeshlibrary;
+
+namespace publiclibrary
+{
+    public class MYDerivedClass : MyBaseClass
+    {
+        public void MyDerivedClassMethod()
+        {
+            int a = 10;
+            // b = 20;
+            c = 20;
+            //d = 40;
+            e = 60;
+        }
+    }
+    public class MyOtherClass
+    {
+        public void MyOtherClassmethod()
+        {
+            MYDerivedClass mb = new MYDerivedClass();
+            mb.a = 10;
+            //mb.b = 20;
+            //mb.c = 20;
+            //mb.d = 40;
+            //mb.e = 60;
+        }
+    }      
+}
+
