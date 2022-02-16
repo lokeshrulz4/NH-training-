dba21cff3e2782c509d74bdea2fbf96ccc4e7404
lokeshrulz4 <lokeshrulz4@gmail.com>
2022-02-17 01:34:00 +0530

    day 18 submission

---
+++ day 18 assignments/day18 project1/day18 project1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mathematicslibrary;
+
+namespace day18_project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter number");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(algebra.Factorial(n));
+            Console.ReadLine();
+        }
+    }
+}
+
+        
+    
---
+++ day 18 assignments/day18project2/mathematicallibraryTests/algrbraTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mathematicallibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicLibrary.Tests
+{
+    [TestClass()]
+        public class AlgebraTests
+        {
+            [TestMethod()]
+            public void FactorialTest_Zero_input()
+            {
+                //Arrange
+                int n = 0;
+                int expected = 1;
+                //Act
+                int actual = algrbra.Factorial(n);
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+            [TestMethod()]
+            public void FactorialTest_One_To_Seven_Input()
+            {
+                //Arrange
+                int n = 5;
+                int expected = 120;
+                //Act
+                int actual = algrbra.Factorial(n);
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+            [TestMethod()]
+            public void FactorialTest_greater_than_Seven_Input()
+            {
+                //Arrange
+                int n = 9;
+                int expected = -999;
+                //Act
+                int actual = algrbra.Factorial(n);
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod()]
+            public void FactorialTest_Negitive_Input()
+            {
+                //Arrange
+                int n = -3;
+                int expected = -9999;
+                //Act
+                int actual = algrbra.Factorial(n);
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+        }
+    }
+
+
+    
