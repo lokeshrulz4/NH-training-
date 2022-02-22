3d683516397ba0846adc4199cc680089fa437e9e
lokeshrulz4 <lokeshrulz4@gmail.com>
2022-02-23 01:30:54 +0530

    final project submission

---
+++ project submission/MyFinalProject/BusinessLogicLibrary/EmployeeBll.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAcessLayer;
+
+namespace BusinessLogicLibrary
+{
+    public class EmployeeBll
+    {
+        public static bool AddEmployee(int empId, string empName, int empSalary, int empAge)
+        {
+            var result = EmployeeDAL1.AddEmployee(empId, empName, empSalary, empAge);
+
+
+            return result;
+        }
+        public static List<string> GetEmployeeById(int id)
+        {
+            var result = EmployeeDAL1.GetEmployeeById(id);
+            return result;
+        }
+        public static List<string> GetEmployeeByname(string name)
+        {
+            var result = EmployeeDAL1.GetEmployeeByname(name);
+            return result;
+        }
+        public static string[] GetallEmployees()
+        {
+            var result = EmployeeDAL1.GetallEmployees();
+            return result;
+        }
+    }
+}
